7985561ef8f6699ea0be811e5eb1f4cbb0cc8d6c
Terry Tosh <tmt103@txstate.edu>
2023-10-03 09:10:03 -0500

    navbar styling revert point

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// Components
+import './style.css'
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <h1>
+          Terry's Personal Site ;)
+        </h1>
+        <p>
+          Some filler text...
+        </p>
+      </main>
     </div>
   );
 }
